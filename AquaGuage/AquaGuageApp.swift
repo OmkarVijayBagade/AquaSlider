d2548f80cd798d04778fcdef246f5bd87937aa3c
omkar <omkarbagade16@gmail.com>
2025-11-20 22:55:30 +0530

    Initial Commit

---
+++ AquaGuage/AquaGuageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaGuageApp.swift
+//  AquaGuage
+//
+//  Created by Omkar Vijay Bagade on 20/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AquaGuageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
